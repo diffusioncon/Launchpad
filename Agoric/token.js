1d617f29a65484d5935751e7a887ae74bddac052
Theo Turner <theo_turner@outlook.com>
2019-10-01 15:28:32 +0100

    Agoric: add token example

--- Agoric/token.js
+++ Agoric/token.js
@@ -1,4 +1,18 @@
 import { makeMint } from './ERTP/core/issuers';
 
-const baytownBucksMint = makeMint('BaytownBucks');
-const purse = baytownBucksMint.mint(1000, 'community treasury');
+// Create currency and mint 1M tokens
+const token = makeMint('SafeToken');
+const purse = token.mint(1000000, 'treasury');
+
+// Send 10 tokens to Alice
+const paymentForAlice = purse.withdraw(10, 'for alice');
+
+/* Now all you have to do is pass the payment object to Alice, who
+is also an object, as a message. Alice might implement her own
+payment receipt method, so just call that method in your reference
+to her and pass the payment object, e.g. alice.receive(payment). */
+
+// Alice can verify the payment:
+const verifiedPayment = token.claimAll(payment);
+
+// Now, wasn't that simple?
